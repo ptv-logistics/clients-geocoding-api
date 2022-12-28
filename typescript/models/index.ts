d892dd1b62c6e88ce7aa3e0da3cc7e50e6a656e8
MachineUserPTV <github@ptvgroup.com>
2022-12-28 11:02:58 +0000

    Automatic synchronisation from developer.myptv.com

---
+++ typescript/models/index.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Address';
+export * from './Area';
+export * from './CausingError';
+export * from './ErrorResponse';
+export * from './Location';
+export * from './LocationAllOf';
+export * from './LocationProperties';
+export * from './LocationType';
+export * from './LocationsSearchResult';
+export * from './Place';
+export * from './PlaceAllOf';
+export * from './PlaceCategories';
+export * from './PlaceCategory';
+export * from './PlacesByAreaRequest';
+export * from './PlacesSearchResult';
+export * from './Quality';
+export * from './ReferencePosition';
+export * from './RoadAccessPosition';
+export * from './Warning';
